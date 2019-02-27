81a24d1a1339eef7a52cb260a72eddd842498aa9
Tim Knoll <timothy.knoll@gmail.com>
2019-02-27 02:21:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,13 @@
+# class Dog
+#
+#
+#   def initialize (name, breed="Mutt")
+#     @name = name
+#     @breed = breed
+#   end
+# end
+
 class Dog
-
-
   def initialize (name, breed="Mutt")
     @name = name
     @breed = breed
